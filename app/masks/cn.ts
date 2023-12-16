90c75eb0675c091de6fc903d2c8fdf7c76f0d632
luluxiaoyu <ljy.xiaoyu@qq.com>
2023-12-16 20:45:10 +0800

    12

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -16,7 +16,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 300000,
+      max_tokens: 2000,
       presence_penalty: 0,
       frequency_penalty: 0,
       sendMemory: true,
